90a477702649df4351825d3be7646520f5ef8772
Carlo <tabayagcarlo94@gmail.com>
2022-03-15 15:11:39 +0800

    first commit

---
+++ src/main/java/com/atmobileshop/atmobileshoppsql/AtmobileshopPsqlApplication.java
@@ -0,0 +1,13 @@
+package com.atmobileshop.atmobileshoppsql;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AtmobileshopPsqlApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AtmobileshopPsqlApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/atmobileshop/atmobileshoppsql/AtmobileshopPsqlApplicationTests.java
@@ -0,0 +1,13 @@
+package com.atmobileshop.atmobileshoppsql;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AtmobileshopPsqlApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
